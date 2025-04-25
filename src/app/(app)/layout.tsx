b477b4b0c9f5c9062831bbf8abe3d4d660e22dcc
M-HamzaSiddiqui <userhamza01@gmail.com>
2025-04-26 02:53:38 +0530

    meta tags added.

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,14 +2,21 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anonylytics",
-  description: "Smart Data Insights",
+  title: "Anonylytics | Build Forms. Gain Insights. Stay Anonymous.",
+  description:
+    "Create quizzes, feedback forms, and surveys with ease. Get smart visual insights across form categories — all while keeping responses anonymous.",
+  keywords:
+    "anonylytics, anonymous forms, feedback tool, quiz builder, survey analytics, form insights, response analytics, data visualization",
+  authors: [{ name: "Anonylytics Team" }],
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
@@ -19,14 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.png" />
-      </head>
       <AuthProvider>
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Toaster />
+        <body className={inter.className}>
+          <Navbar />
+          {children}
+          <Toaster />
         </body>
       </AuthProvider>
     </html>
